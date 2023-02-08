4c8cbfd3fc6279de7c17dc5a4edaf7d8c43f5009
LuisArgFer <aarguedas810@gmail.com>
2023-02-08 00:10:38 -0600

    se añade el primer workshop

---
+++ fifapp/index.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+app.use(express.json())
+const userRoute=require('./Controllers/teamController');
+const TeamModel = require("./Models/teamModel");
+
+app.use('/api',userRoute);
+const db = mongoose.connect("mongodb://127.0.0.1:27017/fifapp").then(()=>console.log('funca'))
+
+
+app.listen(3000,() => console.log('Fifa app listening on port 3000!'))

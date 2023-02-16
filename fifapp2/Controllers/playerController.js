48d77bce6c72351dfc8a3029a348d796f17a77c1
LuisArgFer <aarguedas810@gmail.com>
2023-02-15 20:13:45 -0600

    se agrega tercer workshop

--- fifapp2/Controllers/playerController.js
+++ fifapp2/Controllers/playerController.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Player = require('../Models/playerModel');
+const Team = require('../Models/teamModel');
 
-// Get all teams
+
 router.get("/player", (req, res) => {
     Player.model.find({}, (error, player) => {
         if (error) {
@@ -12,7 +13,7 @@ router.get("/player", (req, res) => {
     });
 });
 
-// Get a single team
+
 router.get("/player/:id", (req, res) => {
     Player.model.findById(req.params.id, (error, player) => {
         if (error) {
@@ -25,29 +26,104 @@ router.get("/player/:id", (req, res) => {
     });
 });
 
-// Create a new team
-router.post("/player", (req, res) => {
-    const newPlayer = new Player.model(req.body);
-    newPlayer.save((error) => {
-        if (error) {
-            return res.status(500).send(error);
-        }
-        res.status(201).json(newPlayer);
-    });
-});
 
-// Update a team
-router.put("/player/:id", (req, res) => {
-    Player.model.findByIdAndUpdate(req.params.id, req.body, { new: true }, (error, player) => {
-        if (error) {
-            return res.status(500).send(error);
+//router.post("/player", (req, res) => {
+  //  const newPlayer = new Player.model(req.body);
+    //newPlayer.save((error) => {
+      //  if (error) {
+        //    return res.status(500).send(error);
+        //}
+        //res.status(201).json(newPlayer);
+    //});
+//});
+
+router.post('/player', async function (req, res) {
+    const player = new Player.model();
+    player.name = req.body.name;
+    player.anos = req.body.anos;
+   
+    //find the team
+    try {
+      const teamFound = await Team.model.findById(req.body.team);
+      if (teamFound) {
+        player.team = teamFound;
+      }
+  
+      // create the player anyway
+      if (player.name && player.anos) {
+        await player.save();
+        res.status(201);//CREATED
+        res.header({
+          'location': `http://localhost:3000/player/?id=${player.id}`
+        });
+        res.json(player);
+      } else {
+        res.status(422);
+        console.log('error while saving the player')
+        res.json({
+          error: 'No valid data provided for player'
+        });
+      }
+  
+    } catch (error) {
+      res.status(422);
+      console.log('error while saving the player', error)
+      res.json({
+        error: 'There was an error creating the player'
+      });
+    }
+  });
+
+
+//router.put("/player/:id", (req, res) => {
+  //  Player.model.findByIdAndUpdate(req.params.id, req.body, { new: true }, (error, player) => {
+    //    if (error) {
+      //      return res.status(500).send(error);
+        //}
+        //if (!player) {
+          //  return res.status(404).send('Team not found');
+        //}
+        //res.status(200).json(player);
+    //});
+//});
+
+router.put('/player/:id', async function (req, res) {
+    try {
+      const playerId = req.params.id;
+      const player = await Player.model.findById(playerId);
+  
+      if (player) {
+        // Update the player information
+        player.name = req.body.name || player.name;
+        player.anos = req.body.anos || player.anos;
+  
+        // Update the team if provided
+        if (req.body.team) {
+          const teamFound = await Team.model.findById(req.body.team);
+          if (teamFound) {
+            player.team = teamFound;
+          }
         }
-        if (!player) {
-            return res.status(404).send('Team not found');
-        }
-        res.status(200).json(player);
-    });
-});
+  
+        // Save the changes
+        await player.save();
+  
+        res.status(200); // OK
+        res.json(player);
+      } else {
+        res.status(404); // Not Found
+        res.json({
+          error: 'Player not found'
+        });
+      }
+    } catch (error) {
+      res.status(422);
+      console.log('error while updating the player', error);
+      res.json({
+        error: 'There was an error updating the player'
+      });
+    }
+  });
 
 // Delete a team
 router.delete("/player/:id", (req, res) => {
--- fifapp2/Models/playerModel.js
+++ fifapp2/Models/playerModel.js
@@ -5,7 +5,10 @@ const team = require('../Models/teamModel');
 const player = new Schema({
     name: {type: String},
     anos: {type: String},
-    team: team.schema
+    team: {type: 
+      team.schema,
+      required:false
+    }
 });
 
 const PlayerModel = mongoose.model('players', player);
